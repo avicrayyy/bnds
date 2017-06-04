5a94f0075795302cc7bec140dc374642f3199ee0
ieatcrayolaaa <dsgbdomingo@gmail.com>
2017-06-04 23:13:00 +0800

    Incoming Message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-  	render html: "¡Hola, Mundo!"
-  end
-
-  def goodbye
-  	render html: "Good Bye, World!"
+  	render html: "Incoming: Best sneaker app in the Philippines! -David"
   end
 
 end
