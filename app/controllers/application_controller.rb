ed2204a719ebf0da6e4a675c03df635dfde7d0f4
ieatcrayolaaa <dsgbdomingo@gmail.com>
2017-06-06 18:18:14 +0800

    Created Welcome Page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def hello
-  	render html: "Incoming: Best sneaker app in the Philippines! -David"
-  end
-
 end
